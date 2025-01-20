e943ba285b1cabd5e105888f8f62e35523ba7ba8
vuphuong1794 <phuongvu1794@gmail.com>
2025-01-20 20:53:06 +0700

    add node cache

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import {faker} from "@faker-js/faker";
 import {ProductSchema} from "./schema.js";
+import NodeCache from "node-cache";
 
 const app = express();
 const port = 3000;
+const nodeCache = new NodeCache();
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -22,36 +24,49 @@ mongoose
     });
 
     const Product = mongoose.model("Product", ProductSchema);
-app.use("/", (req, res) => {
+
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-async function createProduct(count=10) {
-    const products =[];
-
-    for(let i=0; i<count; i++) {
-        const newProduct = new Product({
-            name: faker.commerce.productName(),
-            photo: faker.image.url(),
-            price: faker.commerce.price({min: 1500, max: 8000, dec: 0}),
-            stock: faker.commerce.price({min: 1, max: 100, dec: 0}),
-            category: faker.commerce.department(),
-            createAt: new Date(faker.date.past()),
-            updatedAt: new Date(faker.date.recent()),
-        });
-        products.push(newProduct);
+app.get("/products", async (req, res) => {
+    let products;
+    if(nodeCache.has("products")) {
+        products = JSON.parse(nodeCache.get("products"));
+    } else {
+        products = await Product.find({});
+        nodeCache.set("products", JSON.stringify(products));
     }
+    return res.json(products);
+});
 
-    try {
-        await Product.insertMany(products);
-        console.log("Products created");
-    } catch (error) {
-        console.log(error);
-    }
-    console.log("check the database");
-}
+//create dummy data
+// async function createProduct(count=10) {
+//     const products =[];
 
-createProduct(50);
+//     for(let i=0; i<count; i++) {
+//         const newProduct = new Product({
+//             name: faker.commerce.productName(),
+//             photo: faker.image.url(),
+//             price: faker.commerce.price({min: 1500, max: 8000, dec: 0}),
+//             stock: faker.commerce.price({min: 1, max: 100, dec: 0}),
+//             category: faker.commerce.department(),
+//             createAt: new Date(faker.date.past()),
+//             updatedAt: new Date(faker.date.recent()),
+//         });
+//         products.push(newProduct);
+//     }
+
+//     try {
+//         await Product.insertMany(products);
+//         console.log("Products created");
+//     } catch (error) {
+//         console.log(error);
+//     }
+//     console.log("check the database");
+// }
+
+// createProduct(50);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
